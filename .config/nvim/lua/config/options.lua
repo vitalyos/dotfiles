740063b52a7afb53e29c3eb342bbb6f6f5e62303
vitalyos <vitalyos@users.noreply.github.com>
2024-09-07 22:11:01 +0300

    Initial commit
     * added git config
     * added vim config
     * added lazyvim
     * added bashrc

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+--Set the behavior of tab
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- ser cursorline
+vim.opt.cursorline = true
+vim.opt.relativenumber = false
+vim.opt.number = true
